6102948dfb388ed3db08c09da9859949e6b35185
akshayyp <akshaykanth14@example.com>
2022-07-26 12:18:30 -0400

    ok

--- src/card.js
+++ src/card.js
@@ -1,5 +1,4 @@
-import React ,{useState,useEffect} from 'react'
-
+import React ,{useState,useEffect} from 'react
 function Card() {
     const [user,setUser] = useState([]);
 
@@ -51,4 +50,4 @@ function Card() {
     )
 }
 
-export default Card
+export default card
